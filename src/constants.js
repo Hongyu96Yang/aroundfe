e0b86d520e4a2a03724bcc0a4bb67844c2345e2e
Hongyu Yang <hongyuy96@gmail.com>
2022-02-28 13:42:52 -0800

    around front end

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+export const TOKEN_KEY = 'token';
+export const BASE_URL = 'https://around-dot-around-179500.uc.r.appspot.com';
+// all for 0  keyword for 1 user for 2 in order to search easy
+export const SEARCH_KEY = {
+  all: 0,
+  keyword: 1,
+  user: 2
+};
