4363365d683764a17dcfc55a998c8fcd10e8705a
Hongyu Yang <hongyuy96@gmail.com>
2022-03-10 13:13:40 -0800

    done with own GAE

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const TOKEN_KEY = 'token';
-export const BASE_URL = 'https://around-dot-around-179500.uc.r.appspot.com';
+export const BASE_URL = 'https://around-341203.uw.r.appspot.com';
 // all for 0  keyword for 1 user for 2 in order to search easy
 export const SEARCH_KEY = {
   all: 0,
